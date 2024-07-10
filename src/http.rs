524a80a1bed82a57684c99c135edb1ea4fa767a9
stefanszeke <sekestefan@gmail.com>
2024-07-10 12:56:56 +0200

    add integration test

--- src/http.rs
+++ src/http.rs
@@ -56,7 +56,7 @@ pub struct MyErrorResponse {
 
 #[cfg(test)]
 mod tests {
-    use std::sync::Arc;
+    use std::{net::SocketAddr, sync::Arc};
 
     use crate::auth::auth_state::JwkState;
 
@@ -68,21 +68,23 @@ mod tests {
     };
     use http_body_util::BodyExt;
     use mockito::ServerGuard;
-    use tokio::sync::RwLock;
+    use serde_json::Value;
+    use tokio::{net::TcpListener, sync::RwLock};
     use tower::ServiceExt;
 
     async fn setup_mock_server(
         proxy_status_code: StatusCode,
-        auth_with_response: bool,
     ) -> (Router, ServerGuard) {
         let mut server = mockito::Server::new_async().await;
 
+        #[allow(unused)]
         let public_mock = server
             .mock("GET", "/public")
             .with_status(proxy_status_code.as_u16() as usize)
             .with_body(r#"{"message": "Public route"}"#)
             .create_async()
             .await;
+
 
         let client = reqwest::Client::new();
         let jwks = Arc::new(RwLock::new(None));
@@ -94,14 +96,13 @@ mod tests {
             }),
         };
 
-        let url = server.url();
         let router: Router = crate::http::create_router(state);
         (router, server)
     }
 
     #[tokio::test]
-    async fn public_should_return_200_with_body() {
-        let (router, _server) = setup_mock_server(StatusCode::OK, false).await;
+    async fn mock_public_should_return_200_with_body() {
+        let (router, _server) = setup_mock_server(StatusCode::OK).await;
 
         let response = router
             .oneshot(
@@ -117,5 +118,48 @@ mod tests {
         let body_bytes = response.into_body().collect().await.unwrap().to_bytes();
         let body_str = String::from_utf8(body_bytes.into_iter().collect()).unwrap();
         assert_eq!(&body_str, r#"{"message":"Public route"}"#);
+    }
+
+    async fn start_real_server() -> SocketAddr {
+        let client = reqwest::Client::new();
+        let jwks = Arc::new(RwLock::new(None));
+
+        let state = AppState {
+            jwks_state: Arc::new(JwkState {
+                client: client.clone(),
+                jwks: jwks.clone(),
+            }),
+        };
+
+        let app = crate::http::create_router(state);
+
+        let listener = TcpListener::bind("127.0.0.1:8081").await.unwrap();
+        let addr = listener.local_addr().unwrap();
+
+        tokio::spawn(async move {
+            axum::serve(listener, app.into_make_service())
+                .await
+                .unwrap();
+            println!("Server started at http://localhost:8081");
+        });
+        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+
+        addr
+    }
+
+    #[tokio::test]
+    async fn public_endpoint_should_return_200_with_body() {
+        let addr = start_real_server().await;
+
+        let client = reqwest::Client::new();
+        let response = client
+            .get(format!("http://{}/public", addr))
+            .send()
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+        let body: Value = response.json().await.unwrap();
+        assert_eq!(body, json!({"message": "Public route"}));
     }
 }
